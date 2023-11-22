e6efc3f3967a425ba2859dcd02eee193b0886a39
Seeagarg <89650047+Seeagarg@users.noreply.github.com>
2023-11-22 21:17:35 +0530

    changes apos

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            "Passionate B.Tech student excelling in React.js, merging technical proficiency with teamwork and communication for dynamic web solutions."
+            {`"Passionate B.Tech student excelling in React.js, merging technical proficiency with teamwork and communication for dynamic web solutions."`}`
           </p>
           <div>
             <Link
